2bfeb4154dec76674841bc4480b7cd89d15a6e81
Bárbara Reis <babi_ch89@hotmail.com>
2021-01-02 21:13:49 -0300

    Criando a classe PosicaoXadrez

---
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public int Linha { get; set; }
+        public char Coluna { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
